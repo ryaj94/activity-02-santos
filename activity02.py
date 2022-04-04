9b5a953ef3aa100d1a2ccb81cbc1bd1905ad9457
unknown <nctryryaj2694@gmail.com>
2022-04-04 20:18:39 +0800

    commit

---
+++ activity02.py
@@ -0,0 +1,33 @@
+import random
+
+
+
+print("""
+A Lv. 90 Charizard (Fire/Flying, Sp. Atk: 205) attacks a Lv. 95
+Feraligatr (Water, Sp. Def: 188) with Fire Blast, a Fire-type move
+with a Power of 110 and gains a same-type attack bonus.. It hits
+the target normally but deals a not very effective damage. The
+weather on the field is normal. Given the following conditions,
+determine how much damage Charizards attack will deal.
+""")
+
+print ("""
+Level : 90
+Pokemon Name : Charizard
+Type: Fire/Flying
+Sp. Attack : 205
+Power : Fire Blast 110
+
+
+
+
+Battle with:
+
+Level : 95
+Pokemon name: Feraligator
+Type : Water
+Sp. Defense : 188
+
+
+""")
+
